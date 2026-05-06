bbcce7d30c7f6210fc470939096508a6516630f8
tofochi <geforje@mail.ru>
2026-05-06 23:24:53 +0300

    update env file loaded method

--- bot/config.py
+++ bot/config.py
@@ -4,15 +4,24 @@ from pydantic_settings import BaseSettings
 
 from pathlib import Path
 
-
 BASE_DIR = Path(__file__).parent.parent
+
 
 class Settings(BaseSettings):
     bot_token: str
 
     class Config:
-        env_file = BASE_DIR / ".env" # .env file
-        env_file_encoding = "utf-8"
+        env_file = BASE_DIR / ".env"  # .env file
+        env_file_encoding = "utf-8"   # format
+        extra = "ignore"
         arbitrary_types_allowed = True
 
-settings = Settings()
+def check_settings() -> None:
+    settings = Settings()
+    if settings.bot_token is None:
+        print("Error. Token not set")
+    else:
+        print("Good. Token set")
+
+def load_settings() -> Settings:
+    return Settings()
--- bot/main.py
+++ bot/main.py
@@ -1,19 +1,27 @@
 import asyncio
 import logging
 
+from aiogram.enums import ParseMode
 from aiogram import Bot, Dispatcher, F, types
+from aiogram.client.default import DefaultBotProperties
 
-from config import settings
+from config import load_settings, check_settings
 
 
 logging.basicConfig(level=logging.INFO)
 
-bot = Bot(token=settings.bot_token)
+settings = load_settings()
+
+bot = Bot(token=settings.bot_token, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
 dp = Dispatcher()
 
+
 async def main() -> None:
+    check_settings()
+
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     try:
